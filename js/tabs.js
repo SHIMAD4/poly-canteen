dd79f5c17bf6c9c2c1f5c63475d5440642ffa3c1
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-01-06 11:45:41 +0300

    Added tabs in menu section

---
+++ js/tabs.js
@@ -0,0 +1,26 @@
+let tabcontent = document.querySelectorAll(".product__list"),
+    triggers   = document.querySelectorAll(".js-trigger"),
+    allbtn   = document.querySelector(".all")
+
+function openTab(evt, nameOfTab) {
+
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  for (let i = 0; i < triggers.length; i++) {
+    if (triggers[i].classList.contains('btn-active')) {
+      triggers[i].className = triggers[i].className.replace(" btn-active", "");
+    }
+  }
+
+  console.log(nameOfTab.id)
+  if (nameOfTab.id === 'baking' || nameOfTab.id === 'sweets' || nameOfTab.id === 'drinks' || nameOfTab.id === 'prepared') {
+    allbtn.style.display = 'none'
+  }else {
+    allbtn.style.display = 'block'
+  }
+
+  nameOfTab.style.display = "flex";
+  evt.currentTarget.className += " btn-active";
+}
