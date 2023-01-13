797cfa04e30c6f72a1aab9d2927ae8bb3a43d2ea
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-01-13 11:59:37 +0300

    Added modal hiding && refactoring basket.js

--- js/basket.js
+++ js/basket.js
@@ -1,12 +1,34 @@
+const body = document.querySelector('body');
+const basket = document.querySelector('.background');
+
 const btns = document.querySelectorAll('.add');
 const block = document.querySelector('.basket__wrapper');
 const product = document.querySelector('.description');
 const clearBtn = document.querySelector('.basket__reset');
 
+
+window.addEventListener('click', (event) => {
+  if (event.target.classList.contains('header__basket')        ||
+      event.target.classList.contains('js_header_busket_icon') ||
+      event.target.classList.contains('js_header_busket_svg')  ||
+      event.target.classList.contains('js_header_busket_path')) {
+    if (basket.classList.contains('visually-hidden')) {
+      body.style.overflow = 'hidden'
+      basket.classList.remove('visually-hidden')
+    } else {
+      basket.classList.add('visually-hidden')
+    }
+  }
+  if (event.target.classList.contains('background')) {
+    basket.classList.add('visually-hidden')
+    body.style.overflow = 'auto'
+  }
+})
+
+
 btns.forEach(el => {
   el.addEventListener('click', () => {
     const card = el.parentElement
-
     const productInfo = {
       id     : card.id,
       imgSrc : card.querySelector('.card__photo').getAttribute('src'),
@@ -15,12 +37,21 @@ btns.forEach(el => {
       weight : card.querySelector('.description').querySelector('.weight').innerText,
       count  : card.querySelector('[data-counter]').innerText,
     }
-    
     const itemInBusket = block.querySelector(`[id='${productInfo.id}']`)
     
     if (itemInBusket) {
       const counterElem = itemInBusket.querySelector('[data-counter]')
       counterElem.innerText = parseInt(counterElem.innerText) + parseInt(productInfo.count)
+      let counterBtns = document.querySelectorAll('[data-action]');
+      counterBtns.forEach(el => {
+        el.addEventListener('click', () => {
+          if (el.dataset.action === 'minus') {
+            console.log('Минус')
+          } else {
+            console.log('Плюс')
+          }
+        })
+      })
     } else {
       const cartTemp = `
       <div class="temp" id="${productInfo.id}">
