76e9468315106c4a7f593692b3feb8ae34e324fb
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-05-09 06:53:30 +0300

    fix: fixed the nav in the header, the img in the hero component and the bg-color in the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,11 @@ import logo from '../assets/logo-footer.png';
 
 function Footer() {
     return (
-        <div className="container mx-auto px-20 flex flex-row justify-center items-center bg-[#E0AE95] py-[60px]">
-            <a className='mr-[100px]' href="https://yandex.ru/maps/-/CCUgVBBn0B"><img className='w-[300px]' src={logo} alt="logo" /></a>
-            <p className='text-[20px] text-white block w-[880px]'>Буфет использует файлы cookie. Продолжая работу с этим сайтом, вы подтверждаете использование сайтом cookies вашего браузера, которые помогают нам делать этот сайт удобнее для пользователей. Однако вы можете запретить сохранение файлов cookie в настройках своего браузера.</p>
+        <div className="bg-[#E0AE95]">
+            <div className="container mx-auto px-20 flex flex-row justify-center items-center py-[60px]">
+                <a className='mr-[100px]' href="https://yandex.ru/maps/-/CCUgVBBn0B"><img className='w-[300px]' src={logo} alt="logo" /></a>
+                <p className='text-[20px] text-white block w-[880px]'>Буфет использует файлы cookie. Продолжая работу с этим сайтом, вы подтверждаете использование сайтом cookies вашего браузера, которые помогают нам делать этот сайт удобнее для пользователей. Однако вы можете запретить сохранение файлов cookie в настройках своего браузера.</p>
+            </div>
         </div>
     )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,10 +12,10 @@ function Header() {
     <div className="container mx-auto px-20 pt-3 flex flex-row justify-between items-center">
         <a className='flex flex-row justify-center items-center' href="https://yandex.ru/maps/-/CCUgVBBn0B"><img src={logo} alt="logo" /></a>
         <nav>
-            <ul className='flex flex-row justify-center items-center last:mr-0 w-100'>
+            <ul className='w-100 flex flex-row justify-center items-center last:mr-0 '>
                 {navLinks.map((link, index) => {
-                    return <li className='flex flex-row justify-center items-center mr-10 w-[150px] last:mr-0' key={index}>
-                                <a className='hover:bg-[#e0ae9579] hover:font-semibold p-[10px] rounded-[10px] text-[20px] text-[#6C4132]' href="#">{link}</a>
+                    return <li className='flex flex-row justify-center items-center mr-10 w-[150px] text-[#6C4132] last:text-white' key={index}>
+                                <a className='hover:bg-[#e0ae9579] hover:font-semibold p-[10px] rounded-[10px] text-[20px] last:hover:bg-[#c2968079]' href="#">{link}</a>
                            </li>
                 })}
             </ul>
