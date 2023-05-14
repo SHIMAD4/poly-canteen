bdc1cb1c01f578def2ea036a890ba4ae362adaaa
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-05-14 15:35:22 +0300

    add(Menu.jsx): Switching content in tabs

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
 import searchIcon from '../assets/menu/search-icon.svg';
 import MenuItem from './MenuItem'
 
 function Menu() {
-    const categories = [
+    const [items, setItems] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [filteredItems, setFilteredItems] = useState([]);
+    
+    useEffect(() => {
+      fetch('http://cw35033.tw1.ru/products/')
+        .then((res) => res.json())
+        .then((arr) => {
+          setItems(arr);
+          setFilteredItems(arr);
+        });
+      fetch('http://cw35033.tw1.ru/category/')
+        .then((res) => res.json())
+        .then((arr) => setCategories(arr));
+    }, []);
+    
+    function getCategory(indexBtn) {
+      indexBtn = indexBtn + 1;
+      const categoriesId = categories.map((item) => item.id);
+      const categoryItems = items.filter((item) => item.category === categoriesId[indexBtn - 1]);
+      setFilteredItems(categoryItems);
+    }
+    
+    // Здесь вы можете использовать состояние filteredItems вместо items для отображения списка элементов на UI
+    
+
+    const categoriesBtn = [
         'Популярное',
         'Выпечка',
         'Сладкое и снеки',
@@ -11,7 +38,7 @@ function Menu() {
     ]
 
     return (
-        <div className="bg-[#FFF0E9] h-screen m-[-12px]">
+        <div className="bg-[#FFF0E9] h-[940px] m-[-15px]">
             <div className="menu container mx-auto px-20 grid">
                 <h1 className="mt-[48px] text-[40px] text-[#5C372B] font-semibold">Меню</h1>
                 <div className="search justify-self-end absolute">
@@ -19,17 +46,17 @@ function Menu() {
                     <input className="w-[340px] h-[60px] rounded-[10px] pl-[70px] pr-[20px] placeholder:text-[#E0AE95] placeholder:text-[24px]" type="text" placeholder="Поиск..."/>
                 </div>
                 <ul className='mt-[40px]'>
-                    {categories.map((category, index) => {
+                    {categoriesBtn.map((category, index) => {
                         return <li className='text-center py-[20px] mb-[35px] cursor-pointer border-[1px] border-[#E0AE95] border-solid rounded-[10px]
-                                            hover:bg-white hover:shadow-xl active:bg-[#ffffff86] active:shadow-none last:mb-[0px]' key={index}>
-                                    <a className='text-[20px] text-[#5C372B] font-semibold' href="#">{category}</a>
+                                            hover:bg-white hover:shadow-xl active:bg-[#ffffff86] active:shadow-none last:mb-[0px]' key={index} onClick={() => getCategory(index)}>
+                                    <p className='text-[20px] text-[#5C372B] font-semibold'>{category}</p>
                                 </li>
                     })}
                 </ul>
                 <div className="menu-item w-full h-[410px] flex flex-row justify-between items-center px-[70px] place-self-center">
-                    <MenuItem name="Сендвич" price="190" weight="90"/>
-                    <MenuItem name="Сендвич" price="190" weight="90"/>
-                    <MenuItem name="Сендвич" price="190" weight="90"/>
+                    {
+                        filteredItems.map((item, index) => <MenuItem name={item.name} price={item.price} weight={item.weight} img={item.image} key={index}/>)
+                    }
                 </div>
                 <a className='button w-fit h-fit px-[15px] py-[10px] bg-[#6C4132] rounded-[10px] 
                               text-[20px] text-white font-semibold
--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 import addToBasket from '../assets/menu/add-to-basket-icon.svg';
 
-function MenuItem({name, price, weight}) {
+function MenuItem({name, price, weight, img, key}) {
     return (
-        <div className="w-[270px] h-full bg-white rounded-[20px] flex flex-col justify-end items-center text-center">
+        <div className="w-[270px] h-full bg-white rounded-[20px] flex flex-col justify-end items-center text-center" key={key}>
+            <img className='mb-[50px]' src={img} alt="item image" width={240} />
             <h2 className='mb-[50px] text-[20px] text-[#5C372B] font-semibold'>{name}</h2>
             <div className="w-full flex justify-between mb-[20px] px-[35px]">
                 <p className='text-[20px] text-[#5C372B] font-semibold'>{price} руб.</p>
